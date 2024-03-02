7e35ed0fe81cb00ba5f22dd5dec26f06381a5f07
Alex <zepzdckitetsu69yomi@gmail.com>
2024-03-02 15:30:37 +0300

    commit

--- cards/models.py
+++ cards/models.py
@@ -4,6 +4,12 @@ Lesson 57 - начали осваивать Django ORM
 ID - Django сделает автоматом, он будет первичным ключом, индексом и автоинкрементом
 Отличие charfield от textfield - в количестве символов, которые можно в них хранить
 Charfield - до 255 символов, textfield - без ограничений
+
+class Meta - это вложенный класс, который содержит метаданные модели
+db_table - это имя таблицы в базе данных
+verbose_name - это имя модели в единственном числе
+verbose_name_plural - это имя модели во множественном числе
+они используются для отображения в админке
 """
 from django.db import models
 
@@ -15,3 +21,10 @@ class Card(models.Model):
     views = models.IntegerField(default=0)
     adds = models.IntegerField(default=0)
 
+    class Meta:
+        db_table = 'Cards'
+        verbose_name = 'Карточка'
+        verbose_name_plural = 'Карточки'
+
+    def __str__(self):
+        return f'Карточка {self.question} - {self.answer[:50]}'
