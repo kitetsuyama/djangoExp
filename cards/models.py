d12b9f7e4db3006d1cdba343b15ea511c7ec726a
Alex <zepzdckitetsu69yomi@gmail.com>
2024-03-02 15:28:18 +0300

    commit

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,17 @@
+"""
+Lesson 57 - начали осваивать Django ORM
+
+ID - Django сделает автоматом, он будет первичным ключом, индексом и автоинкрементом
+Отличие charfield от textfield - в количестве символов, которые можно в них хранить
+Charfield - до 255 символов, textfield - без ограничений
+"""
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    question = models.CharField()
+    answer = models.TextField(max_length=5000)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    adds = models.IntegerField(default=0)
+
