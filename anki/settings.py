79ca4e4da02cb062748896f7794b5569d0cc3c2f
Alex <zepzdckitetsu69yomi@gmail.com>
2024-03-03 13:50:56 +0300

    commit

--- anki/settings.py
+++ anki/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',  # Подключение django-extensions Для shell_plus
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
